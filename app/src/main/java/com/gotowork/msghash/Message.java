72c8c4c244ce0ca4466c7f1b091b576c5ba80218
messbees <messbees@gmail.com>
2018-04-23 16:39:19 +0300

    add message class

--- app/src/main/java/com/gotowork/msghash/Message.java
+++ app/src/main/java/com/gotowork/msghash/Message.java
@@ -5,4 +5,42 @@ package com.gotowork.msghash;
  */
 
 public class Message {
+    private String name, text, time, hash;
+    private boolean isPinned;
+
+    public Message(String name, String text) {
+        this.name = name;
+        this.text = text;
+        this.isPinned = false;
+        setTime();
+        setHash();
+    }
+
+    public void pin(){
+        isPinned = true;
+    }
+
+    private void setTime() {
+
+    }
+
+    private void setHash() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getHash() {
+        return hash;
+    }
 }
