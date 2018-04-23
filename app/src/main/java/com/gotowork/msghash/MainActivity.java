667df115f39212dddf85fd705fe6e8d15736051c
messbees <messbees@gmail.com>
2018-04-23 16:13:47 +0300

    add main activity layout

--- app/src/main/java/com/gotowork/msghash/MainActivity.java
+++ app/src/main/java/com/gotowork/msghash/MainActivity.java
@@ -2,12 +2,26 @@ package com.gotowork.msghash;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public EditText editText, editName;
+    public Button buttonSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeComponents();
+
+    }
+
+    private void initializeComponents() {
+        editText = (EditText) findViewById(R.id.editText);
+        editName = (EditText) findViewById(R.id.editName);
+        buttonSend = (Button) findViewById(R.id.buttonSend);
     }
 }
