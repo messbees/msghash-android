a90adc2c1134cb96a045bce7dc3584f0de58f3a7
messbees <messbees@gmail.com>
2018-04-27 19:48:26 +0300

    complete CBOR encoding

--- app/src/main/java/com/gotowork/msghash/Sawtooth.java
+++ app/src/main/java/com/gotowork/msghash/Sawtooth.java
@@ -1,5 +1,8 @@
 package com.gotowork.msghash;
 
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
@@ -7,6 +10,11 @@ import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.SecureRandom;
 import java.security.spec.ECGenParameterSpec;
+import java.util.HashMap;
+import java.util.Map;
+
+import co.nstant.in.cbor.CborBuilder;
+import co.nstant.in.cbor.CborEncoder;
 
 public class Sawtooth {
     public static KeyPair getKeyPair() throws NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException {
@@ -14,6 +22,17 @@ public class Sawtooth {
         ECGenParameterSpec ecGenParameterSpec = new ECGenParameterSpec("secp256k1");
         keyPairGenerator.initialize(ecGenParameterSpec, new SecureRandom());
         return keyPairGenerator.generateKeyPair();
+    }
+
+    public static byte[] encodePayload(String verb, String hash) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        try {
+            new CborEncoder(byteArrayOutputStream).encode(new CborBuilder().addMap().put("Verb", verb).put("Message", hash).end().build());
+        }
+        catch (Exception e) {
+            Toast.makeText(MainActivity.context, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+        }
+        return byteArrayOutputStream.toByteArray();
     }
 
     public static void sendTransaction(String hash) {
