5c322af07aa865aadf31f81142368c7d0e0b7f06
messbees <messbees@gmail.com>
2018-04-28 17:49:21 +0300

    update settings

--- app/src/main/java/com/gotowork/msghash/SettingsActivity.java
+++ app/src/main/java/com/gotowork/msghash/SettingsActivity.java
@@ -4,13 +4,14 @@ import android.app.AlertDialog;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
-
+    public static SettingsActivity context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        context = this;
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
--- app/src/main/java/com/gotowork/msghash/SettingsFragment.java
+++ app/src/main/java/com/gotowork/msghash/SettingsFragment.java
@@ -1,21 +1,36 @@
 package com.gotowork.msghash;
 
 import android.app.AlertDialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.widget.Toast;
+
+import java.nio.charset.StandardCharsets;
+
+import static android.content.Context.MODE_PRIVATE;
+
 
 public class SettingsFragment extends PreferenceFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         addPreferencesFromResource(R.xml.settings);
+
 
         Preference copyPublic = findPreference("copy_public");
         copyPublic.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
-                //TODO copy public key
+                SharedPreferences sharedPreferences = MainActivity.context.getPreferences(MODE_PRIVATE);
+                String text = sharedPreferences.getString("public", "");
+                copy(text);
                 return false;
             }
         });
@@ -24,7 +39,9 @@ public class SettingsFragment extends PreferenceFragment {
         copyPrivate.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
-                //TODO copy private key
+                SharedPreferences sharedPreferences = MainActivity.context.getPreferences(MODE_PRIVATE);
+                String text = sharedPreferences.getString("private", "");
+                copy(text);
                 return false;
             }
         });
@@ -36,10 +53,40 @@ public class SettingsFragment extends PreferenceFragment {
                 AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
                 builder.setTitle(getString(R.string.hash));
                 builder.setMessage(Hashing.getHash("sample"));
+                builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                builder.setNeutralButton(R.string.copy, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        copy(Hashing.getHash("sample"));
+                    }
+                });
                 AlertDialog alertDialog = builder.create();
                 alertDialog.show();
                 return false;
             }
         });
+
+        Preference copyPayload = findPreference("copy_payload");
+        copyPayload.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                byte[] bytes = Sawtooth.encodePayload("verb", "message");
+                String string = new String(bytes, StandardCharsets.UTF_8);
+                copy(string);
+                return false;
+            }
+        });
+    }
+
+    public void copy(String string) {
+        ClipboardManager clipboard = (ClipboardManager)getActivity().getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("simple text", string);
+        clipboard.setPrimaryClip(clip);
+        Toast.makeText(getActivity(), R.string.copied, Toast.LENGTH_LONG).show();
     }
 }
