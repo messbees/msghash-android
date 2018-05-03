f62d2d3cb19a61702130bd00cfdda3773934eb2b
messbees <messbees@gmail.com>
2018-05-03 22:14:21 +0300

    add getTransactionHeader()

--- app/src/main/java/com/gotowork/msghash/Sawtooth.java
+++ app/src/main/java/com/gotowork/msghash/Sawtooth.java
@@ -12,9 +12,13 @@ import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.SecureRandom;
 import java.security.spec.ECGenParameterSpec;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
+import sawtooth.sdk.protobuf.Transaction;
 import sawtooth.sdk.protobuf.TransactionHeader;
 
 import co.nstant.in.cbor.CborBuilder;
@@ -28,10 +32,10 @@ public class Sawtooth {
         return keyPairGenerator.generateKeyPair();
     }
 
-    public static byte[] encodePayload(String verb, String hash) {
+    private static byte[] encodePayload(String verb, String hash) {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         try {
-            new CborEncoder(byteArrayOutputStream).encode(new CborBuilder().addMap().put("Verb", verb).put("Message", hash).end().build()); //TODO: check/fix wrong sha512 hash for encoded payload
+            new CborEncoder(byteArrayOutputStream).encode(new CborBuilder().addMap().put("Verb", verb).put("Message", hash).end().build());
         }
         catch (Exception e) {
             Toast.makeText(MainActivity.context, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
@@ -39,18 +43,44 @@ public class Sawtooth {
         return byteArrayOutputStream.toByteArray();
     }
 
-    public static String getAddress(String string) {
+    private static String getAddress(String string) {
         String prefix = Hashing.getHash("msghash");
         String address = Hashing.getHash(string);
         return prefix + address;
     }
 
-    public static void applyTransaction(int batcherPublicKey, String inputs, String outputs, int payload) {
-        TransactionHeader transactionHeader = TransactionHeader.newBuilder().addInputs(inputs).addOutputs(outputs).build();
+    private static TransactionHeader applyTransaction(String verb, String message) {
+        TransactionHeader.Builder builder = TransactionHeader.newBuilder();
+        String key = MainActivity.getPublicHex();
+        builder.setBatcherPublicKey(key); //TODO: add batcher public key
+        builder.setFamilyName(MainActivity.context.getString(R.string.family_name));
+        builder.setFamilyVersion(MainActivity.context.getString(R.string.family_version));
+        builder.setInputs(0, getAddress(message));
+        builder.setOutputs(0, getAddress(message));
+        int nonce = new Random(Calendar.getInstance().getTimeInMillis()).nextInt(1000000000);
+        builder.setNonce(String.valueOf(nonce));
+        byte[] payload = encodePayload(verb, message);
+        String hashedPayload = "";
+        try {
+            hashedPayload = Hashing.getHash(payload);
+        }
+        catch (NoSuchAlgorithmException e) {
+
+        }
+        builder.setPayloadSha512(hashedPayload);
+        builder.setSignerPublicKey(key);
+        return builder.build();
+    }
+
+    private static void sendTransaction(String publicKey) {
 
     }
 
-    public static void sendTransaction(String publicKey) {
+    public static void pin(String hashedMessage) {
+
+    }
+
+    public static void check(String hashedMessage) {
 
     }
 }
